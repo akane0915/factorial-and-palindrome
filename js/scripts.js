412e7e71da40fa0d5d43d7036d3d16a5a94cffe3
Asia Kane and Jennifer Kinsey <asia@>
2017-03-28 14:17:36 -0700

    add palidrome section to site.  write js code to collect user input as string, store as variable, reverse string

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-  $("form").submit(function(event){
+
+  // Factorial Section
+  $("#factorial").submit(function(event){
     event.preventDefault();
     var userInput = parseInt($("#numberInput").val());
     var factorial = 1;
@@ -10,7 +12,29 @@ $(document).ready(function(){
       countDown --;
     }
 
-    $("#output").text("The answer is: " + factorial);
+    $("#factorial-output").text("The answer is: " + factorial);
   })
+
+  // Palindrome Section
+  $("#palidrome").submit(function(event){
+    event.preventDefault();
+
+    var palInput = $("#palInput").val().toLowerCase();
+    var reversePal = "";
+
+    function reverseString(str){
+      for(var i = str.length - 1; i >= 0; i--){
+        reversePal += str[i];
+      }
+      return reversePal;
+    }
+    console.log(reverseString(palInput));
+    // if(palInput === reversePal){
+    //   alert
+    // }else{
+    //
+    // }
+
+  });
 
 });
